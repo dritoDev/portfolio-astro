f0b3fd12aeb19dd4ce55835ac83ebf3e2f112923
Alejandro Rosa <pedroalejandrorosaperez@gmail.com>
2026-01-30 23:52:30 -0400

    feat: add projects section using data-driven components

---
+++ src/data/projects.js
@@ -0,0 +1,22 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Portfolio Personal",
+    description:
+      "Portafolio personal desarrollado con Astro, HTML, CSS y JavaScript.",
+    tech: ["Astro", "HTML", "CSS", "JavaScript"],
+    image: "/projects/portfolio.png",
+    demo: "https://portfolio.pedrorosa.dev",
+    repo: "https://github.com/dritoDev/portfolio-astro",
+  },
+  {
+    id: 2,
+    title: "Landing Page",
+    description:
+      "Landing page responsive optimizada para dispositivos móviles.",
+    tech: ["HTML", "CSS", "JavaScript"],
+    image: "/projects/landing.png",
+    demo: "#",
+    repo: "#",
+  },
+];
